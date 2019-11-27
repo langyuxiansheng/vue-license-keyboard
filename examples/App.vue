15cfdaf8e37a47bdb2afb6a078d208eae935b6ff
zhouliujun <1096432931@qq.com>
2019-11-27 17:52:04 +0800

    新增适应移动端

--- examples/App.vue
+++ examples/App.vue
@@ -2,8 +2,11 @@
     <div id="app">
         <p>键盘输入的值:  {{ value }}</p>
         <LicenseKeyboard 
+        ref="LicenseKeyboard"
         class="license-keyboard" 
+        :isMobile="isMobile"
         v-model="value"/>
+        <!-- <button @click="init()">{{ isMobile ? '切换成PC端模式' : '切换成移动端模式'}}</button> -->
         <!-- 
             isMobile
             customClass="custom-class"
@@ -12,19 +15,28 @@
             fontColor="chocolate" 
             keyBorderColor="#ff0"
          -->
-        <!-- <div class="demo">
+        <div class="demo">
             <h4 class="title">示例图片</h4>
             <img src="./assets/images/l1.png" alt="车牌汉字输入">
             <img src="./assets/images/l2.png" alt="车牌数字输入">
             <img src="./assets/images/l3.png" alt="车牌数字输入">
-        </div> -->
+            <img src="./assets/images/l4.jpg" alt="车牌数字输入">
+            <img src="./assets/images/l5.jpg" alt="车牌数字输入">
+        </div>
     </div>
 </template>
 <script>
 export default {
     data(){
         return {
-            value: null
+            value: null,
+            isMobile: false
+        }
+    },
+    methods:{
+        init(){
+            this.isMobile = !this.isMobile;
+            this.$refs.LicenseKeyboard.init();
         }
     }
 }
@@ -35,6 +47,9 @@ export default {
     font-size: 1.5rem;
     .demo{
         margin-top: 20%;
+        img{
+            width: 100%;
+        }
     }
 }
 </style>
