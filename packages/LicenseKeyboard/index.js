7a0665101a22d5a98868ed0b5ca8abc644f33edb
zhouliujun <1096432931@qq.com>
2019-11-27 17:52:04 +0800

    新增适应移动端

--- packages/LicenseKeyboard/index.js
+++
@@ -1,7 +0,0 @@
-// 导入组件，组件必须声明 name
-import LicenseKeyboard from './src/main.vue';
-// 为组件添加 install 方法，用于按需引入
-LicenseKeyboard.install = function (Vue) {
-    Vue.component(LicenseKeyboard.name, LicenseKeyboard);
-}
-export default LicenseKeyboard;
